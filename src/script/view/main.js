f14214437ba980199ba2e3e0bfbffac782ea651c
rudiwawa <rudiwawa1980@gmail.com>
2020-07-23 10:42:27 +0700

    fundamental android

---
+++ src/script/view/main.js
@@ -0,0 +1,42 @@
+const main = () => {
+    const searchElement = document.querySelector("#searchElement");
+    const buttonSearchElement = document.querySelector("#searchButtonElement");
+    const clubListElement = document.querySelector("#clubList");
+
+
+    const onButtonSearchClicked = () => {
+        const dataSource = new DataSource(renderResult, fallbackResult);
+        dataSource.searchClub(searchElement.value);
+    };
+
+
+    const renderResult = results => {
+        clubListElement.innerHTML = "";
+        results.forEach(function (club) {
+            const name = club.name;
+            const fanArt = club.fanArt;
+            const description = club.description;
+
+
+            const clubElement = document.createElement("div");
+            clubElement.setAttribute("class", "club");
+
+
+            clubElement.innerHTML = `<img class="fan-art-club" src="  ${fanArt} " alt="Fan Art"> \n 
+                <div class="club-info">\n 
+                <h2>${name}</h2>\n 
+                <p>${description}</p> 
+                </div>;`
+            clubListElement.appendChild(clubElement);
+        })
+    };
+
+
+    const fallbackResult = message =>{
+        clubListElement.innerHTML = "";
+        clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+    };
+
+
+    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+};
